8c66ed4f9f1f1461640f21499f094079840bef8c
mistakelijc <mistake@10.local>
2016-11-22 14:35:53 +0800

    Initial commit

---
+++ app/src/main/java/com/tzj/toucheventdemo/MyTextView.java
@@ -0,0 +1,48 @@
+package com.tzj.toucheventdemo;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.widget.TextView;
+
+/**
+ * Created by mistake on 16-11-7.
+ */
+
+public class MyTextView extends TextView{
+    public static final String TAG = "MyTextView";
+
+    public MyTextView(Context context) {
+        super(context);
+    }
+
+    public MyTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        switch(event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                Log.d(TAG, "dispatch action down");
+                break;
+            case MotionEvent.ACTION_MOVE:
+                Log.d(TAG, "dispatch action move");
+                break;
+            case MotionEvent.ACTION_UP:
+                Log.d(TAG, "dispatch action up");
+                break;
+        }
+        return super.dispatchTouchEvent(event);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return super.onTouchEvent(event);
+    }
+}
